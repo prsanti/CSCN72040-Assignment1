22ecb0907d234b090387ffb96d12c4ae32036da0
Alaba001 <ialaba2673@conestogac.on.ca>
2025-02-07 15:44:10 -0500

    Menu is done

--- assignment1/src/assignment1/Main.java
+++ assignment1/src/assignment1/Main.java
@@ -1,55 +1,74 @@
 package assignment1;
-import java.util.ArrayList; // import the ArrayList class
-import java.util.Scanner;
+
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+        // ArrayLists for datasets
+        ArrayList<Data> trainingData;
+        ArrayList<Data> testingData;
+        ArrayList<Data> unknownData;
 
-	public static void main(String[] args) {
-		// array list of data set
-		ArrayList<Data> trainingData;
-		ArrayList<Data> testingData;
-		ArrayList<Data> unknownData;
+        // File paths
+        String trainingDataPath = "src/data/trainingData.txt";
+        String testingDataPath = "src/data/testingData.txt";
+        String unknownDataPath = "src/data/unknownData.txt";
 
-		// path to files
-		String trainingDataPath = "src/data/trainingData.txt";
-		String testingDataPath = "src/data/testingData.txt";
-		String unknownDataPath = "src/data/unknownData.txt";
-		
-		// initialize file manager
-		FileIO fileManager = new FileIO();
-		
-		// read training data and set to array list
-		trainingData = fileManager.readFile(trainingDataPath);
-		// read testing
-		testingData = fileManager.readFile(testingDataPath);
-		// read unknown
-		unknownData = fileManager.readFile(unknownDataPath);
-		
-		// run nn on unknown data
-//		for (int i = 0; i < unknownData.size(); i++) {
-//			NearestNeighbour nn = new NearestNeighbour();
-//			int orientation = nn.calculateOrientation(unknownData.get(i), trainingData);
-//			
-//			// set label to calculated orientation
-//			unknownData.get(i).setLabel(orientation);
-//		}
-//		
-//		fileManager.writeFile(unknownData);
-		
-		// knn
-		// run on unknown data
-		for (int i = 0; i < unknownData.size(); i++) {
-//			System.out.println("before");
-//			testingData.get(i).print();
-			KNearestNeighbour knn = new KNearestNeighbour(3);
-			int orientation = knn.calculateOrientation(unknownData.get(i), trainingData);
-			
-			// set label to calculated orientation
-			testingData.get(i).setLabel(orientation);
-		}
-		
-		fileManager.writeFile(unknownData);
-		
-	}
+        // Initialize file manager
+        FileIO fileManager = new FileIO();
 
+        // Read data from files
+        trainingData = fileManager.readFile(trainingDataPath);
+        testingData = fileManager.readFile(testingDataPath);
+        unknownData = fileManager.readFile(unknownDataPath);
+
+        // Initialize menu and prompt user
+        Menu menu = new Menu();
+        menu.displayMenu();
+
+        // Select dataset based on user input
+        ArrayList<Data> selectedData = switch (menu.getFileSelect()) {
+            case 1 -> unknownData;
+            case 2 -> testingData;
+            case 3 -> trainingData;
+            default -> {
+                System.out.println("Invalid file selection. Exiting.");
+                System.exit(0);
+                yield null;
+            }
+        };
+
+        // Run classifier based on user selection
+        switch (menu.getClassifierSelect()) {
+            case 1 -> {
+                NearestNeighbour nn = new NearestNeighbour();
+                for (Data data : selectedData) {
+                    int orientation = nn.calculateOrientation(data, trainingData);
+                    data.setLabel(orientation);
+                }
+            }
+            case 2 -> {
+                KNearestNeighbour knn = new KNearestNeighbour(3);
+                for (Data data : selectedData) {
+                    int orientation = knn.calculateOrientation(data, trainingData);
+                    data.setLabel(orientation);
+                }
+            }
+            case 3 -> {
+                AnotherClassifier anotherClassifier = new AnotherClassifier();
+                for (Data data : selectedData) {
+                    int orientation = anotherClassifier.calculateOrientation(data, trainingData);
+                    data.setLabel(orientation);
+                }
+            }
+            case 4 -> {
+                System.out.println("Exiting...");
+                System.exit(0);
+            }
+            default -> System.out.println("Invalid classifier selection.");
+        }
+
+        // Write updated data back to file
+        fileManager.writeFile(selectedData);
+    }
 }
--- assignment1/src/assignment1/Menu.java
+++ assignment1/src/assignment1/Menu.java
@@ -3,45 +3,66 @@ package assignment1;
 import java.util.Scanner;
 
 public class Menu {
-	int classifierSelect;
-	int fileSelect;
-	
-	public Menu() {
-		classifierSelect = 0;
-		fileSelect = 0;
-	}
-	
-	// getters
-	public int getClassifierSelect() {
-		return classifierSelect;
-	}
-	
-	public int getFileSelect() {
-		return fileSelect;
-	}
-	
-	// setters
-	public void setClassifierSelect(int input) {
-		this.classifierSelect = input;
-	}
-	
-	public void setFileSelect(int input) {
-		this.fileSelect = input;
-	}
-	
-	protected void getClassifier() {
+    private int classifierSelect;
+    private int fileSelect;
+    private Scanner scanner;
+
+    public Menu() {
+        classifierSelect = 0;
+        fileSelect = 0;
+        scanner = new Scanner(System.in);
+    }
+
+    // Getters
+    public int getClassifierSelect() {
+        return classifierSelect;
+    }
+
+    public int getFileSelect() {
+        return fileSelect;
+    }
+
+    // Setters
+    public void setClassifierSelect(int input) {
+        this.classifierSelect = input;
+    }
+
+    public void setFileSelect(int input) {
+        this.fileSelect = input;
+    }
+
+    // MEnu to let users select class 
+    public int getClassifierSelection() {
         System.out.println("--- Select Classifier ---");
         System.out.println("1. Nearest Neighbour Classifier");
         System.out.println("2. K Nearest Neighbour Classifier");
         System.out.println("3. Another Classifier");
         System.out.println("4. Exit");
-	}
-	
-	private void getFile() {
+
+        System.out.print("Enter choice: ");
+        classifierSelect = scanner.nextInt();
+        return classifierSelect;
+    }
+
+    // Prompt user to select file
+    public int getFileSelection() {
         System.out.println("--- Select File ---");
-        System.out.println("1. unkownData.txt");
+        System.out.println("1. unknownData.txt");
         System.out.println("2. testingData.txt");
         System.out.println("3. trainingData.txt");
-//        System.out.println("4. Exit");
-	}
+
+        System.out.print("Enter choice: ");
+        fileSelect = scanner.nextInt();
+        return fileSelect;
+    }
+
+    // Display the menu and set selections
+    public void displayMenu() {
+        classifierSelect = getClassifierSelection();
+        if (classifierSelect == 4) {
+            System.out.println("Exiting program...");
+            System.exit(0);
+        }
+        fileSelect = getFileSelection();
+    }
 }
